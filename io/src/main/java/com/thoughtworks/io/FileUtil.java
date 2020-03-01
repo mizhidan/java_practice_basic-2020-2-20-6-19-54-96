57fdc76a32f0221ef40511614908dc1b2aa40305
mizhidan <dj9824@126.com>
2020-03-01 15:32:46 +0800

    [danjie] feat: io write

--- io/src/main/java/com/thoughtworks/io/FileUtil.java
+++ io/src/main/java/com/thoughtworks/io/FileUtil.java
@@ -1,7 +1,13 @@
 package com.thoughtworks.io;
 
+import jdk.internal.util.xml.impl.Input;
+
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 public class FileUtil {
 
@@ -13,6 +19,32 @@ public class FileUtil {
      * 例如把a文件夹(a文件夹下有1.txt和一个空文件夹c)复制到b文件夹，复制完成以后b文件夹下也有一个1.txt和空文件夹c
      */
     public static void copyDirectory(File from, File to) throws IOException {
+        if (!to.exists()) {
+            to.mkdir();
+        }
+        File[] fromDir = from.listFiles();
+        File[] toDir = to.listFiles();
+        while (toDir != null) {
+            for (File toFile : toDir) {
+                toFile.delete();
+            }
+        }
 
+        for (File fromFile : fromDir) {
+            String newFile = File.separator + fromFile.getName();
+            if (fromFile.isFile()) {
+                try (InputStream input = new FileInputStream(fromFile.getPath());
+                     OutputStream output = new FileOutputStream(to.getPath() + newFile)) {
+                    byte[] buffer = new byte[1000];
+                    int length;
+                    while ((length = input.read(buffer)) != -1) {
+                        output.write(buffer,0,length);
+                        output.flush();
+                    }
+                }
+            } else {
+                copyDirectory(fromFile, new File(to.getPath() + newFile));
+            }
+        }
     }
 }
